44c2213cac1472de19518bfcc6d1644aa7fda309
岳欣琪 <yuexinqi@yuexinqideMacBook-Pro.local>
2023-09-11 16:18:40 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -32,3 +32,40 @@ class FizzBuzz {
         }
     }
 }
+
+class FizzBuzz2 {
+
+    public static void main(String[] args) {
+
+        int i = 1; while (i < 100) {
+
+            i = getI(i);
+        }
+    }
+
+    private static int getI(int i) {
+        // Find out which numbers divide i.
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) {
+
+            System.out.println("Fizz Buzz"); i++;
+
+        } else if (divisibleBy3) {
+
+            System.out.println("Fizz"); i++;
+
+        } else if (divisibleBy5) {
+
+            System.out.println("Buzz"); i++;
+
+        } else {
+
+            System.out.println(i); i++;
+
+        }
+        return i;
+    }
+}
